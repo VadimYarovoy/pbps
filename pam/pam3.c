bc11750b8749306c067194af1e09d279cb283583
Egor S. Orlov <esorlov@esorlov.ru>
2024-04-03 15:57:41 +0300

    Update pam3.c
    new variant - change password, etc

--- pam/pam3.c
+++ pam/pam3.c
@@ -1,51 +1,50 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <security/pam_appl.h>
-
-#define MY_PAM_SERVICE "login"
-#define USER "myuser"
-#define PASSWORD "mypassowrd"
-
-static int pam_conversation(int num_msg, const struct pam_message **msg,
-                            struct pam_response **resp, void *appdata_ptr) {
-    char *pass = malloc(strlen(appdata_ptr) + 1);
-    strcpy(pass, appdata_ptr);
-
-    int i;
-
-    *resp = calloc(num_msg, sizeof(struct pam_response));
-
-    for (i = 0; i < num_msg; ++i) {
-        /* Ignore all PAM messages except prompting for hidden input */
-        if (msg[i]->msg_style != PAM_PROMPT_ECHO_OFF)
-            continue;
-
-        /* Assume PAM is only prompting for the password as hidden input */
-        resp[i]->resp = pass;
-    }
-
-    return PAM_SUCCESS;
-}
-
+#include <security/pam_misc.h>
+#include <stdlib.h>
+ 
+static struct pam_conv conv = {
+    misc_conv, /* Conversation function defined in pam_misc.h */
+    NULL /* We don't need additional data now*/
+};
+ 
 int main()
 {
-	struct pam_conv conv = {&pam_conversation, (void *) PASSWORD};
-	pam_handle_t *handle;
-	int startResult, authResult;
-	startResult = pam_start(MY_PAM_SERVICE, NULL, &conv, &handle); 
-	if (startResult != PAM_SUCCESS) {
-		printf("Start -- %s (%d)\n",pam_strerror(handle,startResult),startResult);
-		return 1;
-	}
-    	authResult = pam_authenticate(handle, PAM_SILENT | PAM_DISALLOW_NULL_AUTHTOK);
-	if (authResult != PAM_SUCCESS) {
-		printf("AUTH -- %s (%d)\n",pam_strerror(handle,authResult),authResult);
-		return 1;
-	}
-
-    	pam_end(handle, authResult);
-	printf("AUTH -- %s (%d)\n",pam_strerror(handle,authResult),authResult);
-
-	return 0;
+        pam_handle_t *handle = NULL;
+        const char *service_name = "pam_example";
+        int retval;
+        char *username; /* This will be set by PAM with pam_get_item (see below) */
+ 
+        retval = pam_start(service_name, NULL, &conv, &handle); /* Initializing PAM */
+        if (retval != PAM_SUCCESS){
+                fprintf(stderr, "Failure in pam initialization: %s", pam_strerror(handle, retval));
+                return 1;
+        }
+ 
+        retval = pam_authenticate(handle, 0); /* Do authentication (user will be asked for username and password)*/
+        if (retval != PAM_SUCCESS) {
+                fprintf(stderr, "Failure in pam authentication: %s", pam_strerror(handle, retval));
+                return 1;
+        }
+  
+        retval = pam_acct_mgmt(handle, 0); /* Do account management (check the account can access the system) */
+        if (retval != PAM_SUCCESS) {
+                fprintf(stderr, "Failure in pam account management: %s", pam_strerror(handle, retval));
+                return 1;
+        }
+ 
+        /* We now get the username given by the user */
+        pam_get_item(handle, PAM_USER, (const void **)&username);
+        printf("WELCOME, %s\n", username);
+ 
+        printf("Do you want to change your password? (answer y/n): ");
+        char answer = getc(stdin); /* Taking user answer */
+        if (answer == 'y') {
+                retval = pam_chauthtok(handle, 0); /* Do update (user will be asked for current and new password) */
+                if (retval != PAM_SUCCESS) {
+                        fprintf(stderr, "Failure in pam password: %s", pam_strerror(handle, retval));
+                        return 1;   
+                }
+        }
+ 
+        pam_end(handle, retval); /* ALWAYS terminate the pam transaction!! */
 }
